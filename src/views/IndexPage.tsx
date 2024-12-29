107b6fafb531829f083d14a53355992751f4e94b
Benjasx <benjasanchez175@gmail.com>
2024-12-29 13:42:54 -0700

    Error en solicitud a la api

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,21 @@
-
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useMemo } from "react";
+import { useAppStore } from "../stores/useAppStore";
 
 export default function indexPage() {
+  const drinks = useAppStore((state) => state.drinks)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const hasDrinks = useMemo(() => drinks.drinks.length,[])
   return (
     <>
-      <div className="mx-auto container px-5">Index</div>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+      {hasDrinks ? (
+        <>
+          <p>Si hay bebidas</p>
+        </>
+      ): (
+        <p className="my-10 text-center text-2xl">No hay</p>
+      )}
     </>
   );
 }
