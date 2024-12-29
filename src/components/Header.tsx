453ca2ff56db127032b015003c68b7e75b39b1a9
Benjasx <benjasanchez175@gmail.com>
2024-12-28 19:42:13 -0700

    Mostrando contenido dependiendo de la pagina y agregando el form de busquedas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
 
   const  {pathname} = useLocation();
-  console.log(pathname)
+  const isHome = useMemo(()=> pathname === '/', [pathname])
   return (
     <header className="bg-slate-800">
         <div className="mx-auto container px-5 py-16">
@@ -20,6 +21,40 @@ export default function Header() {
                   }>Favoritos</NavLink>
                 </nav>
             </div>
+
+            {isHome && (
+              <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                <div className="space-y-4">
+                  <label 
+                    htmlFor="ingredient"
+                    className="block text-white uppercase font-extrabold text-lg"
+                  >Nombre o Ingredientes</label>
+
+                  <input 
+                    id="ingredient"
+                    type="text" 
+                    name="ingredient"
+                    className="p-3 w-full rounded-lg focus:outline-none" 
+                    placeholder="Nombre o Ingediente. Ej. Vodka, Tequila, Café"
+                  />
+                </div>
+                <div className="space-y-4">
+                  <label 
+                    htmlFor="ingredient"
+                    className="block text-white uppercase font-extrabold text-lg"
+                  >Categoria</label>
+
+                  <select 
+                    id="category"
+                    name="category"
+                    className="p-3 w-full rounded-lg focus:outline-none" 
+                  >
+                  <option value="">-- Seleccione --</option>
+                  </select>
+                </div>
+                <input type="submit" value={'Buscar Resetas'} className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"/>
+              </form>
+            )}
         </div>
     </header>
   )
